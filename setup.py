17724706411b990693b2380a485579e26fa21d5d
Jiazhen Zhu <jason.jz.zhu@gmail.com>
2025-08-08 15:27:06 -0400

    Improve PyPI package metadata
    - Load full README.md content as long_description instead of truncated text
    - Add GitHub URL for better project visibility
    - Improve description from generic to specific functionality
    - Add relevant keywords for better discoverability
    - Add Database and Scientific/Engineering topic classifiers
    - Add python_requires specification

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='databathing',
     version='0.2.1',
-    description="build spark job based on query",
+    description="Convert SQL queries to PySpark DataFrame operations",
     author="Jiazhen Zhu",
     author_email="jason.jz.zhu@gmail.com",
+    url="https://github.com/jason-jz-zhu/databathing",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        "Programming Language :: SQL","Programming Language :: Python :: 3.7",
+        "Topic :: Database",
+        "Topic :: Scientific/Engineering",
+        "Programming Language :: SQL",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         'License :: OSI Approved :: MIT License'],
@@ -19,6 +26,8 @@ setuptools.setup(
     install_requires=[
           'mo-sql-parsing',
       ],
-    long_description='# Convert SQL to Spark Code!\n\n[![PyPI Latest Release]',
-    long_description_content_type='text/markdown'
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['sql', 'spark', 'pyspark', 'etl', 'data', 'parser', 'converter'],
+    python_requires=">=3.7",
 )
